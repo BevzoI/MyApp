e1c1af781524b1004530c2b64bc0a39dc10275f2
BevzoI <bevzo.i.n@gmail.com>
2025-01-23 20:24:39 +0100

    HW1

--- App.js
+++ App.js
@@ -1,19 +1,36 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { useFonts } from "expo-font";
+import LoginScreen from "./src/screens/LoginScreen";
+import RegistrationScreen from "./src/screens/RegistrationScreen";
+import { ActivityIndicator, StyleSheet, View } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  const [isLoginScreen, setIsLoginScreen] = useState(false);
+  const [fontsLoaded] = useFonts({
+    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+    "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
+    "Roboto-Light": require("./assets/fonts/Roboto-Light.ttf"),
+    "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return (
+      <View style={style.section}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
+  return isLoginScreen ? (
+    <LoginScreen onRegister={() => setIsLoginScreen(false)} />
+  ) : (
+    <RegistrationScreen onHasAccount={() => setIsLoginScreen(true)} />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
+const style = StyleSheet.create({
+  section: {
     flex: 1,
-    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
   },
