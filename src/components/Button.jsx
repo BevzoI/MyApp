fb2fca1ff93ac540b841652cafbbbff60d2c708f
BevzoI <bevzo.i.n@gmail.com>
2025-01-25 11:27:30 +0100

    task 2

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,43 +1,21 @@
-import React from "react";
-import PropTypes from "prop-types";
 import { TouchableOpacity, StyleSheet } from "react-native";
 import { colors } from "../../styles/global";
 
-const Button = ({ children, onPress, buttonStyle, accessibilityLabel }) => {
+const Button = ({ children, onPress, buttonStyle }) => {
   return (
-    <TouchableOpacity
-      style={[styles.button, buttonStyle]}
-      onPress={onPress}
-      accessibilityRole="button"
-      accessible={true}
-      accessibilityLabel={accessibilityLabel}
-    >
+    <TouchableOpacity style={[style.button, buttonStyle]} onPress={onPress}>
       {children}
     </TouchableOpacity>
   );
 };
 
-Button.propTypes = {
-  children: PropTypes.node.isRequired,
-  onPress: PropTypes.func.isRequired,
-  buttonStyle: PropTypes.object,
-  accessibilityLabel: PropTypes.string,
-};
-
-Button.defaultProps = {
-  buttonStyle: {},
-  accessibilityLabel: "Button",
-};
-
 export default Button;
 
-const styles = StyleSheet.create({
+const style = StyleSheet.create({
   button: {
     borderRadius: 100,
     backgroundColor: colors.orange,
     paddingVertical: 16,
     paddingHorizontal: 32,
-    justifyContent: "center",
-    alignItems: "center",
   },
 });
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import PropTypes from "prop-types";
+import { useState } from "react";
 import { StyleSheet, TextInput, View } from "react-native";
 import { colors } from "../../styles/global";
 
@@ -8,90 +7,46 @@ const Input = ({
   onTextChange,
   placeholder,
   outerStyles,
-  inputStyle,
   rightButton,
   autofocus = false,
   secureTextEntry = false,
-  keyboardType = "default",
-  maxLength,
-  editable = true,
   onBlur: onBlurCustom,
-  accessibilityLabel,
 }) => {
   const [isFocused, setIsFocused] = useState(false);
 
-  const onFocus = () => setIsFocused(true);
+  const onFocus = () => {
+    setIsFocused(true);
+  };
 
   const onBlur = () => {
     setIsFocused(false);
-    if (onBlurCustom) onBlurCustom();
+
+    if (onBlurCustom) {
+      onBlurCustom();
+    }
   };
 
   return (
     <View style={[styles.input, isFocused && styles.focused, outerStyles]}>
       <TextInput
         value={value}
+        autoFocus={autofocus}
         onChangeText={onTextChange}
         placeholder={placeholder}
-        autoFocus={autofocus}
         secureTextEntry={secureTextEntry}
-        keyboardType={keyboardType}
-        maxLength={maxLength}
-        editable={editable}
-        style={[styles.baseText, inputStyle]}
+        style={styles.baseText}
+        autoCapitalize="none"
         onFocus={onFocus}
         onBlur={onBlur}
-        autoCapitalize="none"
-        accessibilityLabel={accessibilityLabel}
       />
+
       {rightButton}
     </View>
   );
 };
 
-Input.propTypes = {
-  value: PropTypes.string,
-  onTextChange: PropTypes.func.isRequired,
-  placeholder: PropTypes.string,
-  outerStyles: PropTypes.object,
-  inputStyle: PropTypes.object,
-  rightButton: PropTypes.element,
-  autofocus: PropTypes.bool,
-  secureTextEntry: PropTypes.bool,
-  keyboardType: PropTypes.oneOf([
-    "default",
-    "email-address",
-    "numeric",
-    "phone-pad",
-    "number-pad",
-  ]),
-  maxLength: PropTypes.number,
-  editable: PropTypes.bool,
-  onBlur: PropTypes.func,
-  accessibilityLabel: PropTypes.string,
-};
-
-Input.defaultProps = {
-  value: "",
-  placeholder: "Enter text",
-  outerStyles: {},
-  inputStyle: {},
-  rightButton: null,
-  autofocus: false,
-  secureTextEntry: false,
-  keyboardType: "default",
-  maxLength: undefined,
-  editable: true,
-  onBlur: null,
-  accessibilityLabel: "Text input field",
-};
-
-export default Input;
-
 const styles = StyleSheet.create({
   input: {
-    flexDirection: "row",
-    alignItems: "center",
     padding: 16,
     height: 50,
     borderRadius: 16,
@@ -100,7 +55,6 @@ const styles = StyleSheet.create({
     backgroundColor: colors.light_gray,
   },
   baseText: {
-    flex: 1,
     fontWeight: "400",
     fontSize: 16,
     lineHeight: 18,
@@ -111,3 +65,5 @@ const styles = StyleSheet.create({
     borderColor: colors.orange,
   },
 });
+
+export default Input;
